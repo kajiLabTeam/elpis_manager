03a53332e6f511b94e6c85a90c1775436030979d
hihumikan <a@qqey.net>
2024-06-27 15:18:14 +0900

    chore: Update init.sql to create tables for Users, rooms, beacons, wifi_access_points, roles, and user_roles

--- init.sql
+++ init.sql
@@ -1,4 +1,47 @@
-CREATE TABLE example (
+CREATE TABLE Users (
+	id INT(20) NOT NULL AUTO_INCREMENT,
+	user_id VARCHAR(20) NOT NULL,
+	password VARCHAR(20),
+	PRIMARY KEY (id)
+);
+
+CREATE TABLE rooms (
+    room_id SERIAL PRIMARY KEY,
+    room_name VARCHAR(100) NOT NULL,
+    location INT(255)
+);
+
+CREATE TABLE beacons (
+    beacon_id SERIAL PRIMARY KEY,
+    beacon_name VARCHAR(100) NOT NULL,
+    service_uuid CHAR(36),
+    mac_address VARCHAR(17),
+    room_id INT,
+    FOREIGN KEY (room_id) REFERENCES rooms(room_id)
+);
+
+CREATE TABLE wifi_access_points (
+    wifi_id SERIAL PRIMARY KEY,
+    ssid VARCHAR(100) NOT NULL,
+    bssid VARCHAR(17) NOT NULL,
+    room_id INT,
+    FOREIGN KEY (room_id) REFERENCES rooms(room_id)
+);
+
+CREATE TABLE roles (
+    role_id INT PRIMARY KEY,
+    role_name VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE user_roles (
+    user_id INT,
+    role_id INT,
+    PRIMARY KEY (user_id, role_id),
+    FOREIGN KEY (user_id) REFERENCES Users(id),
+    FOREIGN KEY (role_id) REFERENCES roles(role_id)
+);
+
+CREATE TABLE query_server (
     id SERIAL PRIMARY KEY,
-    name VARCHAR(100) NOT NULL
+    url VARCHAR(255) NOT NULL
 );
